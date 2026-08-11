fd7d271fa361f9ac8f3d052e473dedc2e5e1e9a6
Yash7639 <192524275.simats@saveetha.com>
2026-08-11 14:21:23 +0530

    Add files via upload

---
+++ LAB EXP/Main (2).java
@@ -0,0 +1,67 @@
+import java.net.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+
+        Thread server = new Thread(() -> {
+            try {
+                ServerSocket ss = new ServerSocket(5000);
+                Socket s = ss.accept();
+
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(s.getInputStream()));
+
+                PrintWriter out = new PrintWriter(
+                        s.getOutputStream(), true);
+
+                String msg;
+
+                while ((msg = in.readLine()) != null) {
+                    System.out.println("Client: " + msg);
+                    out.println("Server received: " + msg);
+
+                    if (msg.equalsIgnoreCase("bye"))
+                        break;
+                }
+
+                s.close();
+                ss.close();
+
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        });
+
+        Thread client = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+
+                Socket s = new Socket("localhost", 5000);
+
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(s.getInputStream()));
+
+                PrintWriter out = new PrintWriter(
+                        s.getOutputStream(), true);
+
+                out.println("Hello Server");
+                System.out.println("Server: " + in.readLine());
+
+                out.println("How are you");
+                System.out.println("Server: " + in.readLine());
+
+                out.println("bye");
+                System.out.println("Server: " + in.readLine());
+
+                s.close();
+
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        });
+
+        server.start();
+        client.start();
+    }
+}
